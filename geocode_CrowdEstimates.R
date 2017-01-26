d7c06ec3d7d22ec25641e142d9ddffbb6f34defa
Brent Brewington <brent.brewington@gmail.com>
2017-01-26 08:31:16 -0500

    Commit 2

--- geocode_CrowdEstimates.R
+++ geocode_CrowdEstimates.R
@@ -2,8 +2,12 @@ library(tidyverse) #ggplot2,tibble,tidyr,readr,purrr,dplyr
 library(ggmap)
 library(jsonlite)
 
-turnout <- read_csv("data/CrowdEstimates.csv") %>% 
+turnout <- read_csv("data/CrowdEstimates.csv", na = character()) %>% 
      mutate(city_state_country = paste(City, State, Country, sep = ", "))
+turnout <- 
+     turnout %>% mutate(State = ifelse(City == "Bangor" & Country == "UK", "Gwynedd", State),
+                        City = ifelse(City == "Milhelm" & State == "PA", "Millheim", City),
+                        State = ifelse(City == "Lilongwe" & Country == "Malawi", "Central Region", State))
 
 locations <- vector(mode = "list", length = nrow(turnout))
 
@@ -17,14 +21,15 @@ locations_flat1 <- vector(mode = "list", length = length(locations))
 for(i in seq_along(locations)){
      print(i)
      if(locations[[i]]$status == "ZERO_RESULTS"){
-          locations_flat1[[i]] <- data.frame(City1 = NA, State1 = NA, Country1 = NA,
-                                             lat = NA, lon = NA)
+          locations_flat1[[i]] <- data.frame(City1 = as.character(NA), State1 = as.character(NA), Country1 = as.character(NA),
+                                             lat = as.numeric(NA), lon = as.numeric(NA))
      } else{
-          locations_flat1[[i]]$City1 <- turnout$City[i]
-          locations_flat1[[i]]$State1 <- turnout$State[i]
-          locations_flat1[[i]]$Country1 <- turnout$Country[i]
-          locations_flat1[[i]]$lat <- locations[[i]]$results[[1]]$geometry$location$lat
-          locations_flat1[[i]]$lon <- locations[[i]]$results[[1]]$geometry$location$lat
+          locations_flat1[[i]] <- 
+               data.frame(City1 = turnout$City[i], State1 = turnout$State[i],
+                     Country1 = turnout$Country[i], 
+                     lat = locations[[i]]$results[[1]]$geometry$location$lat %>% as.numeric(),
+                     lon = locations[[i]]$results[[1]]$geometry$location$lng %>% as.numeric(),
+                     stringsAsFactors = F)
      }
      
 }
